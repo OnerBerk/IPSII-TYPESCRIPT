5e93f6997eb73709c52cdb3f6e296444c090cd3b
OnerBerk <oner.berk@joza-it.fr>
2023-10-16 18:09:23 +0200

    exercices

---
+++ src/exercice-1.ts
@@ -0,0 +1,23 @@
+"use strict";
+
+type User = unknown;
+
+export const users: unknown[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+];
+
+export function logPerson(user: unknown) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
---
+++ tests/exercice-1.test.ts
@@ -0,0 +1,5 @@
+import {add} from "../src/add";
+
+test("test de addition", () => {
+    expect(add(1, 2)).toBe(3)
+})
